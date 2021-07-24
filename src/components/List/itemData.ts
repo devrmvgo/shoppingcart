941de637f068c9dc98ae3fc1487affbb524bc661
Luiz Felipe <felipealvesalps@gmail.com>
2021-07-24 13:08:39 -0300

    added material ui

---
+++ src/components/List/itemData.ts
@@ -0,0 +1,8 @@
+export const itemData = [
+    {
+        img: 'https://img.ibxk.com.br/2020/01/30/30021141299110.jpg?w=1120&h=420&mode=crop&scale=both',
+        title: 'Image',
+        author: 'author',
+    },
+
+];
